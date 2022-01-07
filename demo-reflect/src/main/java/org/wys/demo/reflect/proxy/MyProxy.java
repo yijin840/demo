9c3d8f7f59034c3138a2db49bae3de76ebafd482
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-01-07 17:06:43 +0800

    feat: 添加动态代理

--- demo-reflect/src/main/java/org/wys/demo/reflect/proxy/MyProxy.java
+++ demo-reflect/src/main/java/org/wys/demo/reflect/proxy/MyProxy.java
@@ -1,5 +1,8 @@
 package org.wys.demo.reflect.proxy;
 
+import net.sf.cglib.proxy.Proxy;
+import org.wys.demo.reflect.proxy.func.ProxyFunction;
+
 /**
  * @author wys
  * @date 2022/1/6
@@ -7,9 +10,12 @@ package org.wys.demo.reflect.proxy;
 public class MyProxy {
 
     public static void main(String[] args) {
+        ProxyFunction proxy = (ProxyFunction) Proxy.newProxyInstance(ProxyFunction.class.getClassLoader(),
+                new Class[]{ProxyFunction.class},
+                (o, method, objects) -> method.invoke(o, objects));
+        System.out.println(proxy.getResult());
 
     }
-
 
 
 }
---
+++ demo-reflect/src/main/java/org/wys/demo/reflect/proxy/func/UserProxy.java
@@ -0,0 +1,12 @@
+package org.wys.demo.reflect.proxy.func;
+
+/**
+ * @author wys
+ * @date 2022/1/7
+ */
+public class UserProxy implements ProxyFunction {
+    @Override
+    public int getResult() {
+        return 0;
+    }
+}
