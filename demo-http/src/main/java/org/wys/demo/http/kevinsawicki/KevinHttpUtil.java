9fbcfc310af39e565005ff3e740a44d46e3dd189
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-01-16 10:59:30 +0800

    feat： 添加kevin http 包

---
+++ demo-http/src/main/java/org/wys/demo/http/kevinsawicki/KevinHttpUtil.java
@@ -0,0 +1,36 @@
+package org.wys.demo.http.kevinsawicki;
+
+
+import com.github.kevinsawicki.http.HttpRequest;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+/**
+ * @author wys
+ * @date 2022/1/13
+ */
+public class KevinHttpUtil {
+
+    private void streamRead() {
+        byte[] bytes = new byte[2048];
+        try(BufferedInputStream buffer = HttpRequest.get("https://youtube.com").buffer()) {
+            while ((buffer.read(bytes, 0, bytes.length)) != -1) {
+                String s = new String(bytes);
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getRead() {
+        String body = HttpRequest.get("https://youtube.com").body();
+        System.out.println(body);
+    }
+    public static void main(String[] args) {
+        KevinHttpUtil kevinHttpUtil = new KevinHttpUtil();
+        kevinHttpUtil.getRead();
+    }
+
+}
---
+++ demo-test/src/main/java/org/wys/demo/test/OutputStreamTest.java
@@ -0,0 +1,26 @@
+package org.wys.demo.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author wys
+ * @date 2022/1/14
+ */
+public class OutputStreamTest {
+
+    public static void main(String[] args) throws IOException {
+        OutputStream outputStream = "123123abcabc";
+        byte[] bytes = new byte[1024];
+        outputStream.write(bytes);
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        byte[] read = new byte[1024];
+        int cnt;
+        while ((cnt = bis.read(read)) != -1) {
+            System.out.println((char) cnt);
+        }
+    }
+
+}
