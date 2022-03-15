f7a1392417fef6917ea83d3970f1906d76fc167d
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-03-15 12:03:02 +0800

    temp

---
+++ demo-test/src/main/java/org/wys/demo/test/StringTest.java
@@ -0,0 +1,21 @@
+package org.wys.demo.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wys
+ * @date 2022/3/14
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        List<String> strs = new ArrayList<>();
+        while (true) {
+            strs.add("aaaa");
+            strs.add("bbbb");
+            strs.add("cccd");
+            strs.add("dddd");
+        }
+    }
+}
