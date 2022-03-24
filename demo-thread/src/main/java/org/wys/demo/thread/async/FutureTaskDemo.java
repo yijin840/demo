3c5f61bdea004a667dc022711d0573beda37e22a
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-03-24 15:27:25 +0800

    feat: 添加futureTaskDemo类

---
+++ demo-thread/src/main/java/org/wys/demo/thread/async/FutureTaskDemo.java
@@ -0,0 +1,51 @@
+package org.wys.demo.thread.async;
+
+import java.util.concurrent.*;
+import java.util.function.Supplier;
+
+/**
+ * @author wys
+ * @date 2022/3/23
+ */
+public class FutureTaskDemo {
+
+    private final ExecutorService executor = new ThreadPoolExecutor(5,
+            5,
+            1,
+            TimeUnit.MINUTES,
+            new LinkedBlockingQueue<>(),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTaskDemo demo = new FutureTaskDemo();
+        CountDownLatch count = new CountDownLatch(2);
+        Integer f1 = demo.asyncExecute(count, () -> {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += i;
+            }
+            count.countDown();
+            return sum;
+        });
+        Integer f2 = demo.asyncExecute(count, () -> {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += i;
+            }
+            count.countDown();
+            return sum;
+        });
+        count.await();
+        System.out.println(f1 + f2);
+    }
+
+    private <T> T asyncExecute(CountDownLatch latch, Supplier<T> supplier) throws ExecutionException, InterruptedException {
+        Future<T> future = executor.submit(() -> {
+            latch.countDown();
+            return supplier.get();
+        });
+        return future.get();
+    }
+
+}
