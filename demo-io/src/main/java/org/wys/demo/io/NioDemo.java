ef700218e7a3368130a196b311f06fbb067b8041
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-04-21 10:39:45 +0800

    temp

---
+++ demo-io/src/main/java/org/wys/demo/io/NioDemo.java
@@ -0,0 +1,17 @@
+package org.wys.demo.io;
+
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+
+/**
+ * @author wys
+ * @date 2022/4/20
+ */
+public class NioDemo {
+
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer;
+
+    }
+
+}
