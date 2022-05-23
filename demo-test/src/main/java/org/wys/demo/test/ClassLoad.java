32537da86af4111629208cd03daba6c9cc4b0c51
wys <wangyuanshuai.wys@alibaba-inc.com>
2022-05-23 17:40:30 +0800

    temp

--- demo-test/src/main/java/org/wys/demo/test/ClassLoad.java
+++ demo-test/src/main/java/org/wys/demo/test/ClassLoad.java
@@ -1,6 +1,6 @@
 package org.wys.demo.test;
 
-/**
+/**-
  * @author wys
  * @date 2022/5/6
  */
@@ -8,5 +8,9 @@ public class ClassLoad {
 
     public static void main(String[] args) {
         System.out.println(ClassLoad.class.getClassLoader().getClass().getClassLoader());
+    }
+
+    private static void loadClass(String className) {
+        System.out.println(className);
     }
 }
