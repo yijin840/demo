34df2fb75191de1ed323412b915e66d40a82dd29
wyscoder <2900086967@qq.com>
2022-06-09 12:29:30 +0800

    temp

--- demo-design/src/main/java/org/wys/demo/design/other/response/SimpleResponse.java
+++ demo-design/src/main/java/org/wys/demo/design/other/response/SimpleResponse.java
@@ -1,7 +1,21 @@
 package org.wys.demo.design.other.response;
 
+import cn.hutool.http.HttpUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import okhttp3.*;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.util.StopWatch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wys
@@ -11,5 +25,13 @@ import lombok.EqualsAndHashCode;
 @Data
 public class SimpleResponse extends BaseResponse {
     private static final long serialVersionUID = -3727680121138319629L;
+
+    public static void main(String[] args) throws IOException {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            double v = r.nextDouble() * (new Random().nextBoolean()?-1:1);
+            System.out.println(v);
+        }
+    }
 
 }
